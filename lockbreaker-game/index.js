6dcc85f3793d82221157a9dd4522eac765dc5a23
time4nothing <dchamlin@gmail.com>
2024-08-03 17:26:32 -0400

    started validate/reset routes

--- lockbreaker-game/index.js
+++ lockbreaker-game/index.js
@@ -2,24 +2,38 @@ import express from 'express';
 
 const app = express();
 const port = 7001;
+
 let locked = true;
 let stage = 2;
+let name = '';
 
 app.use('/static', express.static('./public')); // set location for static files
-app.use(express.urlencoded()); // parse submitted data from frontend
+app.use(express.urlencoded({ extended: true })); // parse submitted data from frontend
 
 // initial page render
 app.get('/', (req, res) => {
-    res.render('index.ejs', { lockStatus: locked, stage: stage });
+    res.render('index.ejs', { name: name, lockStatus: locked, stage: stage });
 });
 
 // collect submitted name and unlock keypad
 app.post('/', (req, res) => {
-    console.log(req.body);
-    if (req.body.name) {
+    name = req.body.name;
+    if (name) {
         locked = false;
     }
-    res.render('index.ejs', { lockStatus: locked, stage: stage });
+    res.render('index.ejs', { name: name, lockStatus: locked, stage: stage });
+});
+
+app.post('/validate', (req, res) => {
+    console.log(req.body, "Enter clicked");
+    res.render('index.ejs', { name: name, lockStatus: locked, stage: stage });
+});
+
+app.post('/reset', (req, res) => {
+    console.log("reset pressed");
+    name = "";
+    locked = true;
+    res.render('index.ejs', { name: name, lockStatus: locked, stage: stage });
 });
 
 app.listen(port, () => {
--- lockbreaker-game/public/scripts/script.js
+++ lockbreaker-game/public/scripts/script.js
@@ -1,16 +1,39 @@
 const buttons = document.getElementsByClassName("keypad-button");
 const lights = document.getElementsByClassName("stagelights");
 
+// reset game when reset button clicked
+document.getElementById('reset').addEventListener('click', reset);
+
+async function reset() {
+    try {
+        await fetch("/reset", {
+            method: "POST"
+        })
+        document.getElementById("output").textContent = "";
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
 // activate keypad buttons if not locked
 if (!locked) {
     Array.from(buttons).forEach(button => {
-        button.addEventListener("click", () => {
+        button.addEventListener("click", async () => {
             const element = document.getElementById("output");
             let output = element.textContent;
 
             if (button.id === "enter") {
-                // add submit code
-                console.log("enter was hit");
+                try {
+                    await fetch("/validate", {
+                        method: "POST",
+                        headers: { 'Content-Type': 'application/json' },
+                        body: { guess: element }
+                    });
+                }
+                catch (err) {
+                    console.log(err);
+                }
             } else if (button.id === "backspace") {
                 output = output.slice(0, -1);
                 element.textContent = output;
@@ -31,9 +54,9 @@ if (!locked) {
 Array.from(lights).forEach(light => {
     const stage = light.id.charAt(light.id.length - 1);
     if (Number(stage) <= currentStage) {
-        console.log(Number(stage), currentStage);
+        //console.log(Number(stage), currentStage);
         document.getElementById(`light${currentStage}`).setAttribute('class', 'light-on');
-        console.log(document.getElementById(`light${currentStage}`).getAttribute('class'));
+        //console.log(document.getElementById(`light${currentStage}`).getAttribute('class'));
     }
-    console.log(document.getElementById(`light${currentStage}`).getAttribute('class'));
+    //console.log(document.getElementById(`light${currentStage}`).getAttribute('class'));
 })
